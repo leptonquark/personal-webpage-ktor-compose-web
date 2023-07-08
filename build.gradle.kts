9d5870bcf3dba5c4017ce63750512b45ac9cd394
Justin Saler <justin.saler@topgolf.com>
2023-07-08 20:33:45 +0200

    Update MainView Styling slightly

--- build.gradle.kts
+++ build.gradle.kts
@@ -55,7 +55,6 @@ kotlin {
                 implementation(compose.runtime)
                 implementation(compose.ui)
                 implementation(compose.foundation)
-                implementation(compose.material)
                 implementation(compose.material3)
             }
         }
--- src/commonMain/kotlin/MainView.kt
+++ src/commonMain/kotlin/MainView.kt
@@ -1,27 +1,34 @@
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 
 @Composable
 fun MainView(about: String) {
-    Column(
-        modifier = Modifier.fillMaxSize().padding(horizontal = 32.dp),
-        verticalArrangement = Arrangement.spacedBy(16.dp, Alignment.CenterVertically),
-        horizontalAlignment = Alignment.CenterHorizontally,
-    ) {
-        Text(
-            text = "About me",
-            textAlign = TextAlign.Center,
-            fontSize = 24.sp,
-        )
-        Text(
-            text = about,
-            textAlign = TextAlign.Center,
-        )
+    MaterialTheme {
+        Column(
+            modifier = Modifier.fillMaxSize().padding(horizontal = 32.dp),
+            verticalArrangement = Arrangement.spacedBy(16.dp, Alignment.CenterVertically),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Text(
+                text = "About me",
+                textAlign = TextAlign.Center,
+                style = MaterialTheme.typography.headlineMedium,
+
+            )
+            Text(
+                text = about,
+                textAlign = TextAlign.Center,
+                style = MaterialTheme.typography.bodyMedium,
+                )
+        }
     }
 }
