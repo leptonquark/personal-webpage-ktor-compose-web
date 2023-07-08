5431b6ae6a8734ed9451572c0a467a3ca5439319
Justin Saler <justin.saler@topgolf.com>
2023-07-08 11:25:19 +0200

    Update MainView Styling slightly

--- src/commonMain/kotlin/MainView.kt
+++ src/commonMain/kotlin/MainView.kt
@@ -1,10 +1,27 @@
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun MainView(about: String) {
-    Column {
-        Text(text = about)
+    Column(
+        modifier = Modifier.fillMaxSize().padding(horizontal = 32.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp, Alignment.CenterVertically),
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Text(
+            text = "About me",
+            textAlign = TextAlign.Center,
+            fontSize = 24.sp,
+        )
+        Text(
+            text = about,
+            textAlign = TextAlign.Center,
+        )
     }
 }
