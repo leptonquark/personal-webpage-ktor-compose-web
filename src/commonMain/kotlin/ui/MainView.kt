a429cf2207cb5f81b9615fb06526f8e9e3b2f42b
Justin Saler <justin.saler@topgolf.com>
2023-07-11 21:57:18 +0200

    Fix detekt in Common

--- src/commonMain/kotlin/ui/MainView.kt
+++ src/commonMain/kotlin/ui/MainView.kt
@@ -1,6 +1,10 @@
 package ui
 
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
