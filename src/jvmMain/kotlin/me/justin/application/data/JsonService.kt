f3c5f353fed60b486ae39dccf6da2d3eaea89d7a
Justin Saler <justin.saler.r@gmail.com>
2023-07-30 14:24:54 +0200

    Use projects-sample as backup

--- src/jvmMain/kotlin/me/justin/application/data/JsonService.kt
+++ src/jvmMain/kotlin/me/justin/application/data/JsonService.kt
@@ -1,8 +1,14 @@
 package me.justin.application.data
 
+import java.net.URL
+
 class JsonService {
 
-    private val projectsResource = javaClass.getResource("/projects.json")
+    private val projectsResource = getProjectsResource()
     val projects: String?
         get() = projectsResource?.readText()
+
+    private fun getProjectsResource(): URL? = javaClass.run {
+        getResource("/projects.json") ?: getResource("/projects-sample.json")
+    }
 }
