09483cebf2cb103503f77bbc18a3b65ef764ce3d
Justin Saler <justin.saler.r@gmail.com>
2023-07-31 18:51:31 +0200

    Add working TopBar

--- src/commonMain/kotlin/ui/MainView.kt
+++ src/commonMain/kotlin/ui/MainView.kt
@@ -2,6 +2,7 @@ package ui
 
 import about.ui.AboutMe
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
@@ -10,7 +11,10 @@ import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.GridItemSpan
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -20,6 +24,7 @@ import project.Project
 import project.ui.ProjectCard
 import ui.unit.Spacing
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainView(
     about: String?,
@@ -28,23 +33,28 @@ fun MainView(
     onContactMeClicked: (ContactMeLink) -> Unit
 ) {
     MaterialTheme {
-        LazyVerticalGrid(
-            columns = GridCells.Fixed(2),
-            modifier = Modifier.fillMaxSize().padding(horizontal = Spacing.M),
-            verticalArrangement = Arrangement.spacedBy(Spacing.S, Alignment.CenterVertically),
-            horizontalArrangement = Arrangement.spacedBy(Spacing.S, Alignment.CenterHorizontally),
+        Scaffold(
+            topBar = { Text("Top bar") },
         ) {
-            item(span = { GridItemSpan(maxLineSpan) }) { about?.let { AboutMe(it) } }
-            item(span = { GridItemSpan(maxLineSpan) }) { Spacer(modifier = Modifier.height(Spacing.M)) }
-            items(projects) { ProjectCard(project = it) }
-            item(span = { GridItemSpan(maxLineSpan) }) { Spacer(modifier = Modifier.height(Spacing.M)) }
-            item(span = { GridItemSpan(maxLineSpan) }) {
-                ContactMe(
-                    links = contactMeLinks,
-                    onContactMeClicked = onContactMeClicked
-                )
+            Box(Modifier.fillMaxSize().padding(horizontal = Spacing.M)) {
+                LazyVerticalGrid(
+                    columns = GridCells.Fixed(2),
+                    modifier = Modifier.fillMaxSize().padding(horizontal = Spacing.M),
+                    verticalArrangement = Arrangement.spacedBy(Spacing.S, Alignment.CenterVertically),
+                    horizontalArrangement = Arrangement.spacedBy(Spacing.S, Alignment.CenterHorizontally),
+                ) {
+                    item(span = { GridItemSpan(maxLineSpan) }) { about?.let { AboutMe(it) } }
+                    item(span = { GridItemSpan(maxLineSpan) }) { Spacer(modifier = Modifier.height(Spacing.M)) }
+                    items(projects) { ProjectCard(project = it) }
+                    item(span = { GridItemSpan(maxLineSpan) }) { Spacer(modifier = Modifier.height(Spacing.M)) }
+                    item(span = { GridItemSpan(maxLineSpan) }) {
+                        ContactMe(
+                            links = contactMeLinks,
+                            onContactMeClicked = onContactMeClicked
+                        )
+                    }
+                }
             }
         }
     }
 }
-
