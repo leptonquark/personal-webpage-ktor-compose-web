057de20b4f8e747550891a437aa9e1213685f216
Justin Saler <justin.saler.r@gmail.com>
2023-08-26 22:28:01 +0200

    clean up WindowClass code

--- app/src/jsWasmMain/kotlin/ui/WindowClass.kt
+++ app/src/jsWasmMain/kotlin/ui/WindowClass.kt
@@ -1,13 +1,15 @@
 package ui
 
+private const val COMPACT_TO_MEDIUM = 600
+private const val MEDIUM_TO_EXPANDED = 840
+
 enum class WindowClass {
     Compact, Medium, Expanded;
 
     companion object {
-        @Suppress("MagicNumber")
         operator fun invoke() = when (windowWidth) {
-            in 0..<600 -> Compact
-            in 600..840 -> Medium
+            in 0..<COMPACT_TO_MEDIUM -> Compact
+            in COMPACT_TO_MEDIUM..MEDIUM_TO_EXPANDED -> Medium
             else -> Expanded
         }
     }
