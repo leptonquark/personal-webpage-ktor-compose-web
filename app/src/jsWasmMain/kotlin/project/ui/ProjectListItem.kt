00387eb79afcc7633d2de8b84e358853f071bea8
Justin Saler <justin.saler.r@gmail.com>
2023-09-03 14:57:02 +0200

    Fix detekt

--- app/src/jsWasmMain/kotlin/project/ui/ProjectListItem.kt
+++ app/src/jsWasmMain/kotlin/project/ui/ProjectListItem.kt
@@ -1,8 +1,13 @@
 package project.ui
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.*
-import androidx.compose.material3.*
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ListItem
+import androidx.compose.material3.LocalContentColor
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.BlendMode
--- app/src/jsWasmMain/kotlin/ui/theme/Color.kt
+++ app/src/jsWasmMain/kotlin/ui/theme/Color.kt
@@ -1,3 +1,4 @@
+@file:Suppress("MagicNumber")
 package ui.theme
 
 import androidx.compose.ui.graphics.Color
